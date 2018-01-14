b132854f3bd176389af67949ad24fff2905b93ad
Reinaldo Santos <reinaldo.java@gmail.com>
2018-01-14 21:29:36 -0200

    update hystrix and zipkin

--- src/main/java/br/com/CRUDHystrix/CrudHystrixApplication.java
+++ src/main/java/br/com/CRUDHystrix/CrudHystrixApplication.java
@@ -1,9 +1,18 @@
 package br.com.CRUDHystrix;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class CrudHystrixApplication {
 
 	public static void main(String[] args) {
