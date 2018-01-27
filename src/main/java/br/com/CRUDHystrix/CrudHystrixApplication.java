e1652d085fce65909e8849bbb22e6a3c7661b438
Reinaldo Santos <reinaldo.java@gmail.com>
2018-01-27 20:50:03 -0200

    update

--- src/main/java/br/com/CRUDHystrix/CrudHystrixApplication.java
+++ src/main/java/br/com/CRUDHystrix/CrudHystrixApplication.java
@@ -3,6 +3,7 @@ package br.com.CRUDHystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 @EnableHystrix
 @EnableHystrixDashboard
+@EnableCaching
 public class CrudHystrixApplication {
 
 	public static void main(String[] args) {
